aac54a005632ab9ddef62d6854dcdf81d3ed13b7
Marcelo Moraes Dermones <marceloh220@hotmail.com>
2017-05-10 18:59:17 -0300

    More updates

--- marcelino/climatizador2.0/mostraVelocidade.h
+++ marcelino/climatizador2.0/mostraVelocidade.h
@@ -35,25 +35,26 @@
 void mostraVelocidade() {
 
   display.set(0, 0);						          //Posiciona cursor na coluna 0 / linha 0
+  display.print("Ventilacao");
+  if (teste.ifset(automatic))
+    display.print("  auto");
+  else
+    display.print("      ");
   uint8_t vel = controle.velocidade();	  //Verifica a velocidade
 
   if (vel == 0) {
-    display.print("Ventilacao      ");
     display.set(0, 1);
     display.print("Desligada       ");
   }
   else if (vel == 1) {
-    display.print("Velocidade      ");
     display.set(0, 1);
     display.print("Baixa           ");
   }
   else if (vel == 2) {
-    display.print("Velocidade      ");
     display.set(0, 1);
     display.print("Media           ");
   }
   else if (vel == 3) {
-    display.print("Velocidade      ");
     display.set(0, 1);
     display.print("Alta            ");
   }
