1c092d46a064737d3ec794060b509f52e2453f39
Marcelo H Moraes <marceloh220@hotmail.com>
2017-09-22 14:44:49 -0300

    Update
    Correção de bugs no algoritimo de leitura do sensor CLZD01 e melhorias
    na estabilidade.

--- Schematics/CLZD01_sourceCode/CLZD01.c
+++ Schematics/CLZD01_sourceCode/CLZD01.c
@@ -27,31 +27,36 @@
     https://www.gnu.org/licenses/gpl-3.0.en.html
 
 ***************************************************************************************************************************/
-
 #include <avr/io.h>			//biblioteca com definicoes do dispositivo
 #include <avr/interrupt.h>	//biblioteca para tratamento de interrupcoes
 #include <stdint.h>			//biblioteca para tipos de veriaveis
 
-#define bv(bit)	(1<<bit)				//criacao de mascaras
-#define data()	(PORTB|=(1<<PB1))		//seta o pino de dado
-#define ndata()	(PORTB&=~(1<<PB1))		//reseta o pino de dado
-#define high()	(PINB&(1<<PB2))			//verifica se pino de clock esta setado
-#define low()	(!(PINB&(1<<PB2)))		//verifica se pino de clock esta resetado
-#define nop()	asm volatile ("nop")	//sem operacao para aguardar sincronismo
+#define bv(bit)	(1<<bit)			//criacao de mascaras
+#define data()	(PORTB|=(1<<PB1))	//seta o pino de dado
+#define ndata()	(PORTB&=~(1<<PB1))	//reseta o pino de dado
+#define high()	(PINB&(1<<PB2))		//verifica se pino de clock esta setado
+#define low()	(!(PINB&(1<<PB2)))	//verifica se pino de clock esta resetado
+
+#define nop(d)	do{asm volatile ("nop");d--;}while(d)	//pequena rotinas de delays
 
 #define maxRead		100	//numero maximo de leituras, depende de quanta memoria RAM existe disponivel
 						//a pilha de ponteiros resiste na memoria ram, deixe espaco para ele
 
 //prototipo de funcoes
 void inicializacao();	//configuracoes iniciais do sistema
-uint16_t leitura();		//leitura do sensor
+uint16_t leitura();		//leituras analogicas
+void mediaMovel();		//retirada de media
 
 //variaveis para leitura de media
-uint16_t lidos[maxRead];	//espaco de leituras anteriores
-uint16_t volatile media;	//valor da leitura media
+uint16_t lidos[maxRead];		//espaco de leituras anteriores
+static uint32_t volatile soma;	//acumulador das leituras analogicas
+uint16_t volatile media;		//valor da leitura media
+static uint8_t  atual;			//interador para o espaco de leituras anteriores
 
 //variavel para envio de dados
-uint8_t interadorEnvio;		//interador para envio dos dados
+static uint8_t interadorEnvio;		//interador para envio dos dados
+static uint16_t volatile envio;		//cache para envio de dados
+static uint8_t paridade;			//para calculo de paridade
 
 //interrupcao de pevencao de erros no envio
 ISR(TIMER0_OVF_vect) {		//se o mestre travar a comunicacao por um tempo muito longo
@@ -62,20 +67,20 @@ ISR(TIMER0_OVF_vect) {		//se o mestre travar a comunicacao por um tempo muito lo
 
 //interrupcao de envio
 ISR(INT0_vect) {		//interrupcao INT0 AVR
-
-	nop();				//espera 1 ciclo (125ns)
+	
+	uint8_t i = 2;
+	nop(i);				//espera algum tempo
    
 	if (high()) {		//teste de clock contra ruidos
 	   
 		TCNT0 = 0;		//quando uma transmissao esta em andamento normal envita o overflow do timer0
-	   
-		//variaveis para envio, mantidas em cache para acesso rapido em AVR
-		static uint16_t volatile envio;	//cache para envio de dados
-		static uint8_t paridade;		//para calculo de paridade
-	   
+	   	   
 		//teste de primeiro envio
-		if (interadorEnvio == 0)	//se primeiro envio
+		if (interadorEnvio == 0) {	//se primeiro envio
 			envio = media;			//carrega temperatura medida no cache de envio
+			paridade = 0;			//inicializa o calculador de paridade
+			TCCR0B = 2;				//inicializa o timer para evitar erros no envio
+		}
 		
 		//teste de 10 bits enviados
 		if ( interadorEnvio < 10) {	//se nao foi enviado os 10 bits
@@ -96,7 +101,8 @@ ISR(INT0_vect) {		//interrupcao INT0 AVR
 		else {							//se foi enviado os 10 bits
 			if ((paridade % 2)) data();	//se paridade for par, o decimo segundo bit sera 0
 			else ndata();				//se paridade for impar, o decimo segundo bit sera 1
-			interadorEnvio = 0;			//reinicia o interador no final do envio dos 10 bits
+			interadorEnvio = 0;			//reinicia o interador de envios para proxima trasnmissao
+			TCCR0B = 0;					//desliga o timer0 ao final de uma trasmissao bem sucedida
 			
 		}//fim do calculo de paridade
 		
@@ -106,21 +112,12 @@ ISR(INT0_vect) {		//interrupcao INT0 AVR
 
 //funcao principal
 int main() {
+   
 	inicializacao();
+   
 	while (1) {
-	   
-		//variaveis de leitura
-		static uint16_t volatile soma;	//acumulador das leituras analogicas
-		static uint8_t  atual;			//interador para o espaco de leituras anteriores
-	
-	        //algoritimo de media movel
-	        uint16_t aux = leitura();	//realiza a leitura do sensor analogico
-		soma -= lidos[atual];			//retira do acumulador uma leitura antiga
-		soma += aux;					//faz o somatorio da nova leitura
-		lidos[atual] = aux;				//salva no espaco de leituras a leitura atual
-		atual++;						//incrementa o interador de leitura
-	   
-		media = soma / maxRead;		//tira a media das leituras
+
+		mediaMovel();
 	   
 		if (atual == maxRead)		//se o interador chegou no final do espaco de leituras
 			atual = 0;				//reinicia o interador
@@ -144,30 +141,55 @@ void inicializacao() {
 
 	//conversor analogico/digital
 	DIDR0 = bv(ADC3D);				//desabilita pino diginal em ADC3(PB3)
-	ADMUX = bv(REFS2)|(3<<REFS0)|3;	//conversor analogico lendo pino ADC3(PB3), tensao de referencia no pino Aref(PB0)
+	ADMUX = bv(REFS2)|(3<<REFS0)	//tensao de referencia interna de 2.3V disponivel no pino PB0
+	        |3;						//conversor analogico lendo pino ADC3(PB3)
 
 	//interrupcao para transmissao
-	MCUCR = bv(ISC01) | bv(ISC00);	//configura interrupcao externa INT para deteccao da borda de subida
+	MCUCR = bv(ISC01)|bv(ISC00);	//configura interrupcao externa INT para deteccao da borda de subida
 	GIMSK = bv(INT0);				//habilita interrupcao externa INT
 
 	//timer0 para evitar bloqueio da transmissao
-	TCCR0B = 2;			//prescale 1/8, overflow em (1/8e6*256*8=256us)
-	TIMSK = bv(TOIE0);	//interrupcao no overflow do timer 0
+	TIMSK = bv(TOIE0);		//interrupcao no overflow do timer 0
 
 	//pinos de comunicacao
-	ndata();		//coloca nivel logico baixo no pino de dados
-	DDRB = bv(PB1);	//configura pino serial de saida de dados
+	ndata();			//coloca nivel logico baixo no pino de dados
+	DDRB = bv(PB1);		//configura pino serial de saida de dados
+	
+	i = 1000;
+	nop(i);			//tempo para o sistema estabilizar
 	
 	//interrupcoes globais
-	sei();		//habilita interrupcoes globais
+	sei();			//habilita interrupcoes globais
+
+	//primeira leitura
+	ADCSRA = bv(ADEN)|3; 		//acd com taxa de amostragem alta, 125mil amostras por segundo
+	
+	while (atual != maxRead)	//nas primeiras 100 leituras utiliza taxa de amostragem alta
+		mediaMovel();
+
+	atual = 0;
+
+	ADCSRA = bv(ADEN)|7; 		//acd com taxa de amostragem baixa, 250 amostras por segundo
 
 }//fim da funcao de inicializacao
 
-//funcao de leitura adc
+//funcao de leitura do sensor
 uint16_t leitura() {
-   
-	ADCSRA = bv(ADEN)|bv(ADSC)|3;	//habilita conversor analogico e inicia conversao
-	while (ADCSRA & bv(ADSC));		//aguarda final da conversao
-	return ADC;						//retorna valor lido no sensor
-	
-}//fim da funcao de leitura adc
+
+	ADCSRA = bv(ADEN)|bv(ADSC);	//habilita conversor analogico e inicia conversao
+	while(ADCSRA&bv(ADSC));		//aguarda final da conversao
+	return ADC;					//retorna valor lido no sensor
+
+}//fim da funcao de leitura do sensor
+
+void mediaMovel() {
+	uint16_t aux = leitura();	//realiza a leitura do sensor
+	   
+	//algoritimo de media movel
+	soma -= lidos[atual];		//retira do acumulador uma leitura antiga
+	soma += aux;				//faz o somatorio da nova leitura
+	lidos[atual] = aux;			//salva no espaco de leituras a leitura atual
+	atual++;					//incrementa o interador de leitura
+	media = soma / maxRead;		//tira a media das leituras
+
+}
