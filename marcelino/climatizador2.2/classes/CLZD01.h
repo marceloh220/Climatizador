e8e129b0516c5c9470637f6c71352bc529d826ce
Marcelo Moraes Dermones <marceloh220@hotmail.com>
2017-09-24 11:46:29 -0300

    Update 24092017

--- marcelino/climatizador2.2/classes/CLZD01.h
+++ marcelino/climatizador2.2/classes/CLZD01.h
@@ -8,7 +8,6 @@ class CLZD: private Digital {
   private:
     uint8_t data, clock;
     uint16_t temp;
-    uint8_t paridade;
 
     void pulse() {
       Digital::write(clock, HIGH);
@@ -18,27 +17,27 @@ class CLZD: private Digital {
     }
 
     void com() {
-      uint16_t aux = 0;
+      uint16_t volatile aux = 0;
+      uint8_t paridade = 0;
       uint8_t j = 9;
       for (int i = 0; i < 10; i++) {
         this->pulse();
         if (Digital::read(data)) {
           _delay_us(1);
           if (Digital::read(data)) {
-            aux |= (1 << j);
-            this->paridade++;
+            aux |= (1<<j);
+            paridade++;
           }
         }
         j--;
       }
       this->pulse();
-      this->paridade %= 2;
-      if (this->paridade == Digital::read(data)) {
+      paridade %= 2;
+      if (paridade == Digital::read(data)) {
         _delay_us(1);
-        if (this->paridade == Digital::read(data))
+        if (paridade == Digital::read(data))
           this->temp = aux;
       }
-      this->paridade = 0;
     }
 
   public:
